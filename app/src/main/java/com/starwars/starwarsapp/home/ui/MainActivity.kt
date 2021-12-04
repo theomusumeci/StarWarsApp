8f9411760806c17858ec86ac0d1c057143deee64
TMusumeci <tmusumeci@yopapp.me>
2021-12-04 17:22:12 -0300

    First commit

---
+++ app/src/main/java/com/starwars/starwarsapp/home/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.starwars.starwarsapp.home.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.starwars.starwarsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
